5de37f1f4f01355096feb96361aeadf55bb4994b
danisasmita <ngabdikegede@gmail.com>
2026-05-11 21:27:04 +0700

    feat: add SSL and memory limits to backend config and optimize Docker image using Alpine

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -8,8 +8,17 @@ module.exports = {
   dialect: process.env.DB_DIALECT || "mysql",
   timezone: "-03:00",
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 3306,
   database: process.env.DB_NAME,
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
-  logging: false
+  logging: false,
+  ...(process.env.DB_SSL === 'true' && {
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  })
 };
